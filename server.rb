aeb9290394464239cbb6b9df697c56c63c284a7b
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-11-06 15:31:02 -0800

    completed response

---
+++ server.rb
@@ -0,0 +1,50 @@
+# server.rb
+
+require 'socket'
+
+def parse_request(request_line)
+  url_components = request_line.split(/[ ?]/)
+
+  http_method = url_components[0]
+  path = url_components[1]
+  params = url_components[2].split('&').map do |param|
+    param.split('=')
+  end.to_h
+
+  [http_method, path, params]
+end
+
+server = TCPServer.new('localhost', 3003)
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
+
+  http_method, path, params = parse_request(request_line)
+  rolls = params['rolls'].to_i
+  sides = params['sides'].to_i
+
+  # output to console
+  puts request_line
+  puts http_method
+  puts path
+  puts params
+
+  # server response
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts '<html>'
+  client.puts '<body>'
+  client.puts '<pre>'
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts '</pre>'
+  rolls.times { client.puts '<p>', rand(sides) + 1, '</p>' }
+  client.puts '</body>'
+  client.puts '</html>'
+
+  client.close
+end
